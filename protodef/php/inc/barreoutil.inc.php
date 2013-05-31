c0a0c7aeb46f6cbaafbaba055bc8e10a778f3fc0
Louis <louis.amore.iut2@gmail.com>
2013-05-31 11:19:46 +0200

    Suppression ancien prototype

--- protodef/php/inc/barreoutil.inc.php
+++
@@ -1,6 +0,0 @@
-		<?php require_once("liaison.modal.inc.php"); ?>
-<div class="row-fluid boutMenu">
-	
-		<button type="submit" class="btn outil liaison pull-right menuTools" data-target="#liaisonModal" data-toggle="modal"><i class="icon-wrench "></i>Livrets de liaison</button>	
-	
-</div>
